dceaaf8fd7fed979459f6e192ef714454f38970c
devbrunarodrigues <devbrunarodrigues@gmail.com>
2025-05-08 19:07:12 +0100

    classificação do imc e criacao de uma nova tela result

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -37,7 +38,15 @@ class MainActivity : AppCompatActivity() {
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
 
-                println("Ação do botão" + resultado) }
+                val intent = Intent(this, ResultActivity::class.java)
+
+                intent.putExtra("KEY_RESULT_IMC", resultado)
+
+
+                startActivity(intent)
+
+                println("Ação do botão" + resultado)
+            }
         }
     }
 }
